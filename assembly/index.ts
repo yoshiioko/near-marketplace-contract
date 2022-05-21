bfe1e937ee1804f332d080c66583943f5878af5b
yoshiioko <acardoza@pdx.edu>
2022-05-20 21:38:21 -0700

    Update: refactor contract to use Product class from model.ts.

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,11 +1,17 @@
-import { PersistentUnorderedMap } from "near-sdk-as";
+import { Product, listedProducts } from "./model";
 
-export const products = new PersistentUnorderedMap<string, string>("PRODUCTS");
-
-export function setProduct(id: string, productName: string): void {
-  products.set(id, productName);
+export function setProduct(product: Product): void {
+  let storedProduct = listedProducts.get(product.id);
+  if (storedProduct !== null) {
+    throw new Error("A product with ${product.id} already exists");
+  }
+  listedProducts.set(product.id, Product.fromPayload(product));
 }
 
-export function getProduct(id: string): string | null {
-  return products.get(id);
+export function getProduct(id: string): Product | null {
+  return listedProducts.get(id);
+}
+
+export function getProducts(): Product[] {
+  return listedProducts.values();
 }
